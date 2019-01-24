206653c52b763b65148729877245896e3bc33e39
krishuyghe <36008639+krishuyghe@users.noreply.github.com>
2019-01-24 22:28:44 +0100

    aanklikbaar gemaakt en verschil player1 player2

--- Dobble/Dobble/Dobble/Pages/TwoPage.xaml.cs
+++ Dobble/Dobble/Dobble/Pages/TwoPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Dobble.hulpclasse;
+﻿using Dobble.Domain;
+using Dobble.hulpclasse;
 using System;
 
 using Xamarin.Forms;
@@ -9,17 +10,26 @@ namespace Dobble.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TwoPage : ContentPage
 	{
-		public TwoPage ()
+        static string basis = Device.RuntimePlatform == Device.Android ? "a" : "Images/a";
+        static string achtervoegsel = Device.RuntimePlatform == Device.Android ? ".png" : ".jpg";
+        public int bas = basis.Length + 6;
+        public int acht = achtervoegsel.Length;
+        MakePlayGround makeplayground = new MakePlayGround();
+       
+        public TwoPage ()
 		{
             var makeplayground = new MakePlayGround();
             var playground = makeplayground.MakePlayField(2, Globals.Level + 2);
-            string basis = Device.RuntimePlatform == Device.Android ? "a" : "Images/a";
-            string achtervoegsel = Device.RuntimePlatform == Device.Android ? ".png" : ".jpg";
-
-            Grid grid = new Grid
+            raster();
+            void raster ()
             {
-                VerticalOptions = LayoutOptions.FillAndExpand,
-                RowDefinitions =
+                
+
+
+                Grid grid = new Grid
+                {
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    RowDefinitions =
                 {
                     new RowDefinition { Height = 40},
                     new RowDefinition { Height = 40},
@@ -31,80 +41,91 @@ namespace Dobble.Pages
                     new RowDefinition { Height = 40},
                     new RowDefinition { Height = 40 }
                 },
-                ColumnDefinitions =
+                    ColumnDefinitions =
                 {
                     new ColumnDefinition { Width = 40 },
                     new ColumnDefinition { Width = 40 },
                     new ColumnDefinition { Width = 40 }
                 }
-            };
-            int n = 0;
-            int rij = 0;
-            int col = 0;
-            do
+                };
+                int n = 0;
+                int rij = 0;
+                int col = 0;
+                do
+                {
+                    var Figuur = basis + playground.Cards[0].picturelist[n] + achtervoegsel;
+                    var imageButton = new ImageButton { HeightRequest = 20, Source = Figuur };
+                    imageButton.Clicked += Player1;
+                    grid.Children.Add(imageButton, rij, col);
+                    rij++;
+                    if (rij > 2)
+                    {
+                        rij = 0;
+                        col++;
+                    }
+                    n++;
+                } while (n < playground.Cards[0].picturelist.Count);
+
+                n = 0;
+                rij = 0;
+                col = 5;
+                do
+                {
+                    var Figuur = basis + playground.Cards[1].picturelist[n] + achtervoegsel;
+                    var imageButton = new ImageButton { HeightRequest = 20, Source = Figuur };
+                    imageButton.Clicked += Player2;
+                    grid.Children.Add(imageButton, rij, col);
+                    rij++;
+                    if (rij > 2)
+                    {
+                        rij = 0;
+                        col++;
+                    }
+                    n++;
+                } while (n < playground.Cards[0].picturelist.Count);
+
+
+
+                // Build the page.
+                this.Content = grid;
+            }
+            
+
+
+            void Player1(object sender, EventArgs e)
             {
-                var Figuur = basis + playground.Cards[0].picturelist[n] + achtervoegsel;
+                ImageButton btn = (ImageButton)sender;
                 
-                grid.Children.Add(new ImageButton { HeightRequest = 20, IsVisible = true, Source = Figuur }, rij, col);
-                rij++;
-                if (rij > 2)
-                {
-                    rij = 0;
-                    col++;
-                }
-                n++;
-            } while (n < playground.Cards[0].picturelist.Count);
+                var gedrukt = btn.Source.ToString();
+                int len = gedrukt.Length;
+                gedrukt = gedrukt.Substring(bas, len - bas - acht);
 
-             n = 0;
-             rij = 0;
-             col = 5;
-            do
+                Zoekoplossing zoekoplossing = new Zoekoplossing();
+                var oplossing = zoekoplossing.Oplossing(playground);
+                DisplayAlert("player1", oplossing, gedrukt);
+                playground = makeplayground.MakePlayField(2, Globals.Level + 2);
+                raster();
+            }
+            void Player2(object sender, EventArgs e)
             {
-                var Figuur = basis + playground.Cards[1].picturelist[n] + achtervoegsel;
+                ImageButton btn = (ImageButton)sender;
+                
+                var gedrukt = btn.Source.ToString();
+                int len = gedrukt.Length;
+                gedrukt = gedrukt.Substring(bas, len - bas - acht);
 
-                grid.Children.Add(new ImageButton { HeightRequest = 20, IsVisible = true, Source = Figuur }, rij, col);
-                rij++;
-                if (rij > 2)
-                {
-                    rij = 0;
-                    col++;
-                }
-                n++;
-            } while (n < playground.Cards[0].picturelist.Count);
+                Zoekoplossing zoekoplossing = new Zoekoplossing();
+                var oplossing = zoekoplossing.Oplossing(playground);
+                DisplayAlert("player2", oplossing, gedrukt);
+                playground = makeplayground.MakePlayField(2, Globals.Level + 2);
+                raster();
+            }
 
 
 
-            // Build the page.
-            this.Content = grid;
-            
 
-            SizeChanged += (object sender, EventArgs args) =>
-            {
-                if (this.Width > 0)
-               {
-                   
-                    var grote = this.Height / 11;
-
-            //        Rij0.Height = grote * 0.9;
-            //        Rij1.Height = grote;
-            //        Rij2.Height = grote;
-            //        Rij3.Height = grote;
-            //        Rij4.Height = grote * 1.35;  
-            //        Rij5.Height = grote;
-            //        Rij6.Height = grote;
-            //        Rij7.Height = grote;
-            //        Rij8.Height = grote * 0.9;
-            //        Col0.Width = grote;
-            //        Col1.Width = grote;
-            //        Col2.Width = grote;
-            //        Col3.Width = grote;
-            //        Col4.Width = grote;
-                }
-
-            };
-            
         }
 
-        
+
     }
 }
