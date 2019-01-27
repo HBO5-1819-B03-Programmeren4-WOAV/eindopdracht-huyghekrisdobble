ae86dd00d5cc5b6c2d392502b40c225eccb4f01f
krishuyghe <36008639+krishuyghe@users.noreply.github.com>
2019-01-27 21:15:42 +0100

    layout aanpassen

--- Dobble/Dobble/Dobble/Pages/TwoPage.xaml.cs
+++ Dobble/Dobble/Dobble/Pages/TwoPage.xaml.cs
@@ -14,20 +14,27 @@ namespace Dobble.Pages
         static string achtervoegsel = Device.RuntimePlatform == Device.Android ? ".png" : ".jpg";
         public int bas = basis.Length + 6;
         public int acht = achtervoegsel.Length;
-       
+      
+      
+        
+
 
         MakePlayGround makeplayground = new MakePlayGround();
-       
-        public TwoPage ()
-		{
+
+        public TwoPage()
+        {
             var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
-            var hoogte = mainDisplayInfo.Height/12;
+            double schermhoogte = Device.RuntimePlatform == Device.Android ? mainDisplayInfo.Height / 3 : mainDisplayInfo.Height;
+
+
+
+            var hoogte = schermhoogte / 16;
             var makeplayground = new MakePlayGround();
             var playground = makeplayground.MakePlayField(2, Globals.Level + 2);
             raster();
-            void raster ()
+            void raster()
             {
-                
+
 
 
                 Grid grid = new Grid
@@ -36,18 +43,19 @@ namespace Dobble.Pages
                     HorizontalOptions = LayoutOptions.Center,
                     RowDefinitions =
                 {
-                    new RowDefinition { Height = hoogte/4},
+                    new RowDefinition { Height = hoogte * 0.9},
                     new RowDefinition { Height = hoogte},
                     new RowDefinition { Height = hoogte},
                      new RowDefinition { Height = hoogte },
-                    new RowDefinition { Height = hoogte/3},
+                    new RowDefinition { Height = hoogte * 1.35},
                     new RowDefinition { Height = hoogte},
                      new RowDefinition { Height = hoogte},
                     new RowDefinition { Height = hoogte},
-                    new RowDefinition { Height = hoogte/4}
+                    new RowDefinition { Height = hoogte * 0.9}
                 },
                     ColumnDefinitions =
                 {
+
                     new ColumnDefinition { Width = hoogte },
                     new ColumnDefinition { Width = hoogte },
                     new ColumnDefinition { Width = hoogte }
@@ -59,7 +67,7 @@ namespace Dobble.Pages
                 do
                 {
                     var Figuur = basis + playground.Cards[0].picturelist[n] + achtervoegsel;
-                    var imageButton = new ImageButton { HeightRequest = 20, Source = Figuur };
+                    var imageButton = new ImageButton {  Source = Figuur };
                     imageButton.Clicked += Player1;
                     grid.Children.Add(imageButton, rij, col);
                     rij++;
@@ -77,7 +85,7 @@ namespace Dobble.Pages
                 do
                 {
                     var Figuur = basis + playground.Cards[1].picturelist[n] + achtervoegsel;
-                    var imageButton = new ImageButton { HeightRequest = 20, Source = Figuur };
+                    var imageButton = new ImageButton {  Source = Figuur };
                     imageButton.Clicked += Player2;
                     grid.Children.Add(imageButton, rij, col);
                     rij++;
@@ -94,38 +102,32 @@ namespace Dobble.Pages
                 // Build the page.
                 this.Content = grid;
             }
-            
+
 
 
             void Player1(object sender, EventArgs e)
             {
                 ImageButton btn = (ImageButton)sender;
+                spel(btn, "player1");
                 
-                var gedrukt = btn.Source.ToString();
-                int len = gedrukt.Length;
-                gedrukt = gedrukt.Substring(bas, len - bas - acht);
-
-                Zoekoplossing zoekoplossing = new Zoekoplossing();
-                var oplossing = zoekoplossing.Oplossing(playground);
-                DisplayAlert("player1", oplossing, gedrukt);
-                playground = makeplayground.MakePlayField(2, Globals.Level + 2);
-                raster();
             }
             void Player2(object sender, EventArgs e)
             {
                 ImageButton btn = (ImageButton)sender;
-                
+                spel(btn, "player2");
+            }
+            void spel(ImageButton btn, string player)
+            {
                 var gedrukt = btn.Source.ToString();
                 int len = gedrukt.Length;
                 gedrukt = gedrukt.Substring(bas, len - bas - acht);
 
                 Zoekoplossing zoekoplossing = new Zoekoplossing();
                 var oplossing = zoekoplossing.Oplossing(playground);
-                DisplayAlert("player2", oplossing, gedrukt);
+                DisplayAlert(player, oplossing, gedrukt);
                 playground = makeplayground.MakePlayField(2, Globals.Level + 2);
                 raster();
             }
-
 
 
 
